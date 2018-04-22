49a277d58ef8986b90dd05be2d87c0e49c707a13
NgoKimPhu <kimphu_2408@yahoo.com.vn>
2018-04-22 20:23:10 +0800

    Fix crawler's repeated-GET-param crawl loop

--- crawler/spiders/main_spider.py
+++ crawler/spiders/main_spider.py
@@ -16,9 +16,10 @@ class MainSpider(scrapy.Spider):
         if -1 < response.url.rfind('?') > response.url.rfind('/'):
             # Output GET
             split = response.url.split('?')
-            params = [param.split('=') for param in split[-1].split('&')]
+            params = [param.split('=')[0] for param in split[-1].split('&')]
             yield FormItem(action=''.join(split[:-1]), method='GET',
-                    inputs=[InputItem(name=p[0]) for p in params if p[0]])
+                    inputs=[InputItem(name=p) for p in params if p])
+        else: params = []
 
         # Crawl forms
         for i,form in enumerate(response.css('form')):
@@ -27,7 +28,9 @@ class MainSpider(scrapy.Spider):
             method = form.xpath('@method').extract_first('GET')
             inputs = [InputItem(name=inp.xpath('@name').extract_first())
                     for inp in form.css('input,select,textarea,button')
-                    if inp.xpath('@name')]
+                    if inp.xpath('@name')
+                    and inp.xpath('@name').extract_first('') not in params]
+            if not inputs: continue
             yield FormItem(action=action.url, method=method, inputs=inputs)
             # Crawl
             yield scrapy.http.FormRequest.from_response(response, formnumber=i)
