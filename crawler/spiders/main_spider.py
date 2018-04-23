8e813f15cfb6ab3044b57fa8b5a7582a0ee5667a
NgoKimPhu <kimphu_2408@yahoo.com.vn>
2018-04-23 21:24:23 +0800

    Crawl redirecting pages; add logging

--- crawler/spiders/main_spider.py
+++ crawler/spiders/main_spider.py
@@ -9,14 +9,20 @@ class MainSpider(scrapy.Spider):
             for url in start_urls]
 
     def parse(self, response):
+        self.logger.debug("(parse) response: status=%d, URL=%s" % (
+                response.status, response.url))
         # Crawl links
-        for a in response.css('a'):
-            yield response.follow(a, callback=self.parse)
+        for href in response.xpath('//a/@href'):
+            self.logger.debug("(parse) a href to %s" % href.extract())
+            yield response.follow(href, callback=self.parse, meta={
+                    'dont_redirect': True,
+                    'handle_httpstatus_list': range(300, 309) })
 
         if -1 < response.url.rfind('?') > response.url.rfind('/'):
             # Output GET
             split = response.url.split('?')
             params = [param.split('=')[0] for param in split[-1].split('&')]
+            self.logger.info("(parse) yield GET form at %s" % response.url)
             yield FormItem(action=''.join(split[:-1]), method='GET',
                     inputs=[InputItem(name=p) for p in params if p])
         else: params = []
@@ -31,8 +37,19 @@ class MainSpider(scrapy.Spider):
                     if inp.xpath('@name')
                     and inp.xpath('@name').extract_first('') not in params]
             if not inputs: continue
+            self.logger.info("(parse) yield %s form to %s" % (method, action))
             yield FormItem(action=action.url, method=method, inputs=inputs)
-            # Crawl
-            yield scrapy.http.FormRequest.from_response(response, formnumber=i)
-            yield scrapy.http.FormRequest.from_response(response, formnumber=i,
+            # Crawl submitted form
+            self.logger.debug("(parse) crawl %s form to %s" % (method, action))
+            request = scrapy.http.FormRequest.from_response(response, formnumber=i)
+            request.meta['dont_redirect'] = True
+            yield request
+            request = scrapy.http.FormRequest.from_response(response, formnumber=i,
                     formdata={ inp['name']: '1' for inp in inputs })
+            request.meta['dont_redirect'] = True
+            yield request
+
+        # Follow redirections after crawl
+        if 299 < response.status < 399 and 'Location' in response.headers:
+            self.logger.debug("(parse) redirecting to %s" % response.headers['Location'])
+            yield response.follow(response.headers['Location'], callback=self.parse)
